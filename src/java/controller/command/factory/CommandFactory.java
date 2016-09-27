4e62bec7f7e62d93d99a2d1cf6c4ac9f9d225b9d
Zakhar Yastrub <zahar.yastrub@gmail.com>
2016-09-27 18:03:23 +0300

    Update CommandFactory.java

--- src/java/controller/command/factory/CommandFactory.java
+++ src/java/controller/command/factory/CommandFactory.java
@@ -6,12 +6,8 @@
 package controller.command.factory;
 
 import controller.commands.Command;
-import controller.commands.user.*;
-import controller.commands.superadmin.*;
 import static controller.constants.ConstantsController.*;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,48 +19,6 @@ import javax.servlet.http.HttpServletResponse;
  * @author Zakhar
  */
 public class CommandFactory {
-    private Map<String, Command> commandMap = new HashMap<String, Command>()
-    {{
-        put(REGISTER_USER, new RegisterFormCommand());
-        put(START_PAGE, new HomePageCommand());
-        put(REGISTER_DO, new RegisterCommand());
-        put(LOGIN_USER, new LoginCommand());
-        put(LOGOUT, new LogoutCommand());
-        put(MORE_ABOUT_PERIODICAL, new PeriodicalUnsignedCommand());
-        put(GET_ACQUAINTED, new PeriodicalUnsignedCommand());
-        put(OPEN_PERIODICAL, new PeriodicalPageCommand());
-        put(OPEN_ARTICLE, new ArticlePageCommand());
-        put(BACK_TO_USER, new UserPageCommand());
-        put(BACK_TO_PERIODICALS, new PeriodicalPageCommand());
-        put(REPLENISH_ACCOUNT, new ReplenishAccountPageCommand());
-        put(REPLENISH, new ReplenishCommand());
-        put(SIGNE_UP, new SignedUpPeriodicalCommand());
-        put(USER, new HomePageCommand());
-        put(CHOOSE_PERIODICAL_TO_WRITE_ARTICLE, new WriteArticlePageCommand());
-        put(SEND_ARTICLE, new WriteArticleCommand());
-        put(VIEW_UNCHECKED_ARTICLE, new UncheckedArticlePageCommand());
-        put(PERMIT_ARTICLE_USER, new PermitArticleUserCommand());
-        put(DELETE_UNPERMITED_ARTICLE_USER, new DeleteUnpermitedArticleUserCommand());
-        put(LOGIN_ADMIN, new LoginSuperAdminCommand());
-        put(LOGOUT_ADMIN, new LogoutSuperAdminCommand());
-        put(CREATE_PERIODICAL, new SetPeriodicalCommand());
-        put(SUPERADMIN, new LoginPageCommand());
-        put(LANGUAGE, new LanguageCommand());
-        put(DELETE_PERIODICAL, new DeletePeriodicalCommand());
-        put(UNSET_ADMIN, new SetAdminFalseCommand());
-        put(DELETE_USER, new DeleteUserCommand());
-        put(SET_ADMIN, new SetAdminTrueCommand());        
-        put(UNBLOCK_USER, new UnblockUserCommand());
-        put(BLOCK_USER, new BlockUserCommand());
-        put(DELETE_PERIODICAL_ARTICLE, new DeletePeriodicalArticlePageCommand());
-        put(DELETE_ARTICLE, new DeleteArticleCommand());
-        put(PERMIT_ARTICLE, new PermitArticleCommand());
-        put(DELETE_UNPERMITED_ARTICLE, new DeleteUnpermitedArticleCommand());
-        put(VIEW_ARTICLE, new ViewArticleCommand());
-        put(PAY_FOR_ARTICLES, new PayForArticlesCommand());
-        put(USER_MANAGEMENT, new UserManagementCommand());
-        put(PERIODICALS_MANAGEMENT, new AdminPageCommand());
-    }};
 
     /**
      * getCommand
